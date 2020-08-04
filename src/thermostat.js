869ef074feee441e8ec7872c7aab3f782cbc82a7
Dafi2 <dafi2@rosies-air.home>
2020-08-04 14:30:45 +0100

    Update object to include a minimum temp' value, add the isMinimumTemperature function

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,6 @@
 function Thermostat() {
     this.temperature = 20;
+    this.MINIMUM_TEMP = 10;
 };
 
 Thermostat.prototype.getCurrentTemperature = function() {
@@ -10,6 +11,14 @@ Thermostat.prototype.up = function() {
     this.temperature += 1;
 }
 
+Thermostat.prototype.isMinimumTemperature = function() {
+    return this.temperature === this.MINIMUM_TEMP;
+}
+
 Thermostat.prototype.down = function() {
-    this.temperature -= 1;
+    if (this.isMinimumTemperature()) {
+        return;
+    } else {
+        this.temperature -= 1;
+    }
 }
