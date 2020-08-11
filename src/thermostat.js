3336a38c2c7d98d31d64fcc326d9ac5eb37b98be
Dafi2 <dafnalibman@gmail.com>
2020-08-12 00:02:42 +0100

    Refactor showEnergyUsage function, add a medium-energy-usage-limit temperature

--- src/thermostat.js
+++ src/thermostat.js
@@ -2,6 +2,7 @@ function Thermostat() {
     this.DEFAULT_TEMPERATURE = 20;
     this.temperature = this.DEFAULT_TEMPERATURE;
     this.MINIMUM_TEMP = 10;
+    this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
     this.MAX_TEMP_PSM_ON = 25;
     this.MAX_TEMP_PSM_OFF = 32;
     this.powerSaveMode = 'on';
@@ -56,11 +57,11 @@ Thermostat.prototype.resetTemperature = function() {
 }
 
 Thermostat.prototype.showEnergyUsage = function() {
-     if (this.temperature < 18) {
+     if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
          return 'low-usage'
-     } else if (this.temperature > 18 && this.temperature < 25) {
+     } 
+     if (this.temperature > this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature < this.MAX_TEMP_PSM_ON) {
          return 'medium-usage'
-     } else {
-         return 'high-usage'
      }
+    return 'high-usage'
  }
